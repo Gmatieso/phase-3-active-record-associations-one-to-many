79f26069507f81aa5444a852deffdd69c9080066
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-07 03:01:36 +0300

    update review to use macro

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,5 @@
 class Review < ActiveRecord::Base
-    # a review belongs to a game
-    def game
-      # self is the review instance
-      Game.find(self.game_id)
-    end
-  
+    #update review to use macro instead of our custom method 
+    belongs_to :game
   end
   
